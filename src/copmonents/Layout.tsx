6568d2c5d2480d6c9f94fef4d76b004a265fdfaa
androidblog <androidblog@yandex.ru>
2023-05-03 13:02:51 +0400

    add numbers to nav

--- src/copmonents/Layout.tsx
+++ src/copmonents/Layout.tsx
@@ -8,14 +8,20 @@ const mainMenu = [
   { name: "Technology", path: "/technology" },
 ];
 
+const convertToDoubleDigit = (number: number) => {
+  return number < 10 ? `0${number}` : number;
+};
 export default function Layout() {
   return (
     <nav>
       <ul>
-        {mainMenu.map((item) => (
-         <li key={item.name}>
-         <Link href={item.path} key={item.name}>{item.name}</Link>
-         </li>
+        {mainMenu.map((item, index) => (
+          <li key={item.name} className="uppercase mx-5">
+            <Link href={item.path} key={item.name}>
+              <span className="mr-3">{convertToDoubleDigit(index)}</span>
+              {item.name}
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
