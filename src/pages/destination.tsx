a4f0d8ae166a074455b8e9cc76e9b3ea8e182675
androidblog <androidblog@yandex.ru>
2023-05-03 13:54:10 +0400

    create blanc pages

---
+++ src/pages/destination.tsx
@@ -0,0 +1,5 @@
+export default function Destination() {
+  return (
+    <div>destination</div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,16 @@ export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+
+    >
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold text-center">
+          SpaceX Launches
+        </h1>
+        <p className="text-xl text-center"> 
+          A list of launches from SpaceX
+        </p>
+      </div>
+    </main>
   );
 }
