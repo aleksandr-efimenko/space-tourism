ca042f96d4a39e43fd8ead180f8881ae0942271f
androidblog <androidblog@yandex.ru>
2023-05-06 13:22:57 +0400

    add background image to the destination page

--- src/pages/destination.tsx
+++ src/pages/destination.tsx
@@ -1,5 +1,19 @@
+import BackgroundImage from "@/components/BackgroundImage";
+import backgroundImage from "~/assets/destination/background-destination-desktop.jpg";
+
 export default function Destination() {
   return (
-    <div>destination</div>
-  )
+    <>
+      <BackgroundImage
+        backgroundImage={backgroundImage}
+        alt="background image"
+      />
+      <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="flex gap-6">
+            <span className="heading5 font-bold opacity-25 text-white">01</span>
+            <p className="heading5 text-white">Pick your destination</p>
+          </h1>
+      </main>
+    </>
+  );
 }
