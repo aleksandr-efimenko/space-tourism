32dc6b26a42704113caab08bcb8b8fca40f8b50c
androidblog <androidblog@yandex.ru>
2023-05-06 13:20:17 +0400

    replace background image with component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,28 @@
+import BackgroundImage from "@/components/BackgroundImage";
+import backgroundImage from "~/assets/home/background-home-desktop.jpg";
 import Image from "next/image";
-import backgroundImage from "~/assets/home/background-home-desktop.jpg"
-
 
 export default function Home() {
-
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      <div className="absolute inset-0 -z-10">
-        <Image src={backgroundImage} alt='background image' fill/>
-      </div>
-
-      <div className="flex flex-col text-white ">
-        <p className="heading5">SO, YOU WANT TO TRAVEL TO</p>
-        <h1 className="heading1">Space</h1>
-        <p className="body-text">
-        Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
-        </p>
-      </div>
-    </main>
+    <>
+      <BackgroundImage
+        backgroundImage={backgroundImage}
+        alt="background image"
+      />
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      >
+        <div className="flex flex-col text-white ">
+          <p className="heading5">SO, YOU WANT TO TRAVEL TO</p>
+          <h1 className="heading1">Space</h1>
+          <p className="">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+      </main>
+    </>
   );
 }
