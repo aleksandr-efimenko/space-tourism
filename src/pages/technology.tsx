a1a57809b7b71cf546285a03f39f3bdf744829d0
androidblog <androidblog@yandex.ru>
2023-05-06 13:28:18 +0400

    add background image and title to the technology page

--- src/pages/technology.tsx
+++ src/pages/technology.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import BackgroundImage from "@/components/BackgroundImage";
+import backgroundImage from "~/assets/technology/background-technology-desktop.jpg";
 
 export default function technology() {
   return (
-    <div>technology</div>
-  )
+    <>
+      <BackgroundImage
+        backgroundImage={backgroundImage}
+        alt="background image"
+      />
+      <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="flex gap-6">
+            <span className="heading5 font-bold opacity-25 text-white">03</span>
+            <p className="heading5 text-white">SPACE LAUNCH 101</p>
+          </h1>
+      </main>
+    </>
+  );
 }
