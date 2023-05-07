234cdeb00c7f89eff3b5f94c740e72a1dcfb81de
androidblog <androidblog@yandex.ru>
2023-05-07 16:45:54 +0400

    add priority to background image

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -10,6 +10,7 @@ export default function BackgroundImage({
   return (
     <div className="absolute inset-0 -z-10">
       <Image
+        priority
         src={backgroundImage}
         alt="background image"
         style={{
