8587b7ef08ef78d2913da54f90744f48fa54b7d2
androidblog <androidblog@yandex.ru>
2023-05-08 14:58:35 +0400

    add mobile styles for heading 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,19 @@
+import { ExploreButton } from '@/components/ExploreButton'
+
 export default function Home() {
   return (
     <>
-      <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24`}
-      >
+      <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
         <div className='flex flex-col'>
           <p className='heading5'>SO, YOU WANT TO TRAVEL TO</p>
           <h1 className='heading1'>Space</h1>
           <p className=''>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
+            Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind
+            of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
           </p>
         </div>
+        <ExploreButton />
       </main>
     </>
-  );
+  )
 }
