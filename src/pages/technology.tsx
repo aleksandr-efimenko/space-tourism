728654181c02c27b80a912ffd2d624e657097c1e
androidblog <androidblog@yandex.ru>
2023-05-10 16:37:47 +0400

    create grid for technology page, add animation

--- src/pages/technology.tsx
+++ src/pages/technology.tsx
@@ -2,6 +2,7 @@ import { NumberedSlider } from '@/components/NumberedSlider'
 import data from '@/data/data.json'
 import { useState } from 'react'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 export default function Technology() {
   const { technology } = data
@@ -9,26 +10,61 @@ export default function Technology() {
 
   return (
     <>
-      <div className='flex flex-col items-center justify-between p-24'>
-        <h1 className='flex gap-6'>
-          <span className='heading5 font-bold opacity-25'>03</span>
-          <p className='heading5'>SPACE LAUNCH 101</p>
-        </h1>
-        <div className='flex'>
-          <NumberedSlider
-            itemsNumber={technology.length}
-            currentIndex={technologyIndex}
-            setIndex={setTechnologyIndex}
-          />
-          <div className='ml-20 flex-1'>
-            <p className='nav-text text-paleblue'>THE TERMINOLOGY…</p>
-            <h2 className='heading2'>{technology[technologyIndex].name}</h2>
-            <p className='text-paleblue'>{technology[technologyIndex].description}</p>
-          </div>
-          <div className=''>
-            <Image alt='space explorer' src={technology[technologyIndex].images.portrait} width={515} height={527} />
+      <div className='page-container pt-52'>
+        <div className='col-start-2 col-end-9'>
+          <h1 className=' flex gap-6'>
+            <span className='heading5 font-bold opacity-25'>03</span>
+            <p className='heading5'>SPACE LAUNCH 101</p>
+          </h1>
+
+          <div className='flex items-center h-5/6'>
+            <div className='flex gap-16'>
+              <NumberedSlider
+                itemsNumber={technology.length}
+                currentIndex={technologyIndex}
+                setIndex={setTechnologyIndex}
+              />
+              <div className='pr-4'>
+                <motion.p
+                  key={technologyIndex + technology[technologyIndex].name}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                  className='nav-text text-paleblue mb-3'
+                >
+                  THE TERMINOLOGY…
+                </motion.p>
+                <motion.h2
+                  key={technology[technologyIndex].name}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.4 }}
+                  className='heading3 mb-4'
+                >
+                  {technology[technologyIndex].name}
+                </motion.h2>
+                <motion.p
+                  key={technology[technologyIndex].description.slice(0, 100)}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.6 }}
+                  className='text-paleblue max-w-[75%]'
+                >
+                  {technology[technologyIndex].description}
+                </motion.p>
+              </div>
+            </div>
           </div>
         </div>
+        <motion.div
+          key={technology[technologyIndex].images.portrait}
+          initial={{ x: '-5rem', opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          className='col-start-9 col-end-13 mt-6'
+        >
+          <Image alt='space explorer' src={technology[technologyIndex].images.portrait} width={515} height={527} />
+        </motion.div>
       </div>
     </>
   )
