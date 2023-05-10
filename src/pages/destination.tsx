f40fb0dd7dfd3d54b8077da7eeefbf9a5f77d231
androidblog <androidblog@yandex.ru>
2023-05-10 15:00:39 +0400

    add animation

--- src/pages/destination.tsx
+++ src/pages/destination.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Image from 'next/image'
 import { TextSlider } from '@/components/TextSlider'
 import { PlanetInfoColumn } from '@/components/PlanetInfoColumn'
+import { motion } from 'framer-motion'
 
 export default function Destination() {
   const { destinations } = data
@@ -16,9 +17,15 @@ export default function Destination() {
             <span className='heading5 font-bold opacity-25 '>01</span>
             <p className='heading5 '>Pick your destination</p>
           </h1>
-          <div className='flex justify-center items-center h-full'>
+          <motion.div
+            key={destinations[destinationIndex].images.webp}
+            initial={{ y: '-5rem', opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className='flex justify-center items-center h-full'
+          >
             <Image src={destinations[destinationIndex].images.webp} alt='destination' width={445} height={445} />
-          </div>
+          </motion.div>
         </div>
 
         <div className='flex flex-col mt-28 col-start-7 col-end-12'>
@@ -27,13 +34,36 @@ export default function Destination() {
             currentIndex={destinationIndex}
             setIndex={setDestinationIndex}
           />
-          <h2 className='heading2  mt-9 mb-[0.875rem]'>{destinations[destinationIndex].name}</h2>
-          <p className='body-text text-paleblue'>{destinations[destinationIndex].description}</p>
-          <hr className='border-gray border-1 mt-[3.375rem] mb-7 ' />
-          <div className='flex gap-20'>
-            <PlanetInfoColumn headerText='AVG. DISTANCE' bodyText={destinations[destinationIndex].distance} />
-            <PlanetInfoColumn headerText='EST. TRAVEL TIME' bodyText={destinations[destinationIndex].travel} />
-          </div>
+          <motion.h2
+            key={destinations[destinationIndex].name}
+            className='heading2  mt-9 mb-[0.875rem]'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.4 }}
+          >
+            {destinations[destinationIndex].name}
+          </motion.h2>
+          <motion.p
+            key={destinations[destinationIndex].description}
+            className='body-text text-paleblue'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6 }}
+          >
+            {destinations[destinationIndex].description}
+          </motion.p>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.8 }}
+            key={destinations[destinationIndex].distance}
+          >
+            <hr className='border-gray border-1 mt-[3.375rem] mb-7 ' />
+            <div className='flex gap-20'>
+              <PlanetInfoColumn headerText='AVG. DISTANCE' bodyText={destinations[destinationIndex].distance} />
+              <PlanetInfoColumn headerText='EST. TRAVEL TIME' bodyText={destinations[destinationIndex].travel} />
+            </div>
+          </motion.div>
         </div>
       </div>
     </>
