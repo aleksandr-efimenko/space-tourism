367c2967827427515fe746d02e3d12c8a409e955
androidblog <androidblog@yandex.ru>
2023-05-14 15:42:57 +0400

    Do not show bg image if string empty

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -6,19 +6,20 @@ type BackgroundImageProps = {
 }
 
 export function BackgroundImage({ backgroundImage, alt }: BackgroundImageProps) {
-  const defaultBackgroundImage = '/assets/home/background-home-desktop.jpg'
   return (
     <div className='absolute inset-0 -z-10'>
-      <Image
-        priority
-        src={backgroundImage ? backgroundImage : defaultBackgroundImage}
-        alt={alt}
-        style={{
-          objectFit: 'cover',
-          objectPosition: 'center',
-        }}
-        fill
-      />
+      {backgroundImage && (
+        <Image
+          priority
+          src={backgroundImage}
+          alt={alt}
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'center',
+          }}
+          fill
+        />
+      )}
     </div>
   )
 }
