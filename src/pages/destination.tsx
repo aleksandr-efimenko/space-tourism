dd50d722b7a5804ffa78b7491bf9cc2d868e4032
androidblog <androidblog@yandex.ru>
2023-05-14 18:41:29 +0400

    fix padding bottom for large screen

--- src/pages/destination.tsx
+++ src/pages/destination.tsx
@@ -22,7 +22,7 @@ export default function Destination() {
         className='w-full text-center
           py-[5.5rem] px-6
           md:py-28 md:px-0
-          lg:pt-52 lg:page-container lg:grid lg:min-h-screen '
+          lg:pt-52 lg:pb-2 lg:page-container lg:grid lg:min-h-screen '
       >
         <div
           className='flex flex-col 
