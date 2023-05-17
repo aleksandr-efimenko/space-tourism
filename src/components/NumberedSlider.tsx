6d436dc0a606f7336fe468cf9af5d9f92485a5ea
androidblog <androidblog@yandex.ru>
2023-05-17 14:53:10 +0400

    add mobile and tablet styles

--- src/components/NumberedSlider.tsx
+++ src/components/NumberedSlider.tsx
@@ -4,14 +4,20 @@ import Link from 'next/link'
 export function NumberedSlider({ itemsNumber, currentIndex, setIndex }: NumberedSliderProps) {
   const items = Array.from(Array(itemsNumber).keys())
   return (
-    <div className='flex flex-col gap-8'>
+    <div
+      className='flex  md:flex-row md:gap-4 
+                        lg:flex-col lg:gap-8'
+    >
       {items.map((index) => (
         <Link
           href='#'
           onClick={() => setIndex(index)}
           key={index}
           className={`heading4 cursor-pointer hover:border-opacity-1 hover:border-white
-          flex justify-center items-center w-20 h-20 border border-white border-opacity-25 rounded-full
+          flex justify-center items-center border border-white border-opacity-25 rounded-full
+          w-10 h-10
+          md:w-[3.75rem] md:h-[3.75rem]
+          lg:w-20 lg:h-20
           ${index === currentIndex ? ' bg-white text-darkspace ' : ''}`}
         >
           {index + 1}
