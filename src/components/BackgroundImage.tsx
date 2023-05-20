b5ec79ede53cddd63b66da8d35405d7bfcfc465d
androidblog <androidblog@yandex.ru>
2023-05-20 19:48:37 +0400

    add placeholder blur to bg images

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -11,6 +11,7 @@ export function BackgroundImage({ backgroundImage, alt }: BackgroundImageProps) 
       {backgroundImage && (
         <Image
           priority
+          placeholder='blur'
           src={backgroundImage}
           alt={alt}
           quality={95}
