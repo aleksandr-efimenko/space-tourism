facbf0ba1def5065f3b943ffd9eb676daf9275ed
androidblog <androidblog@yandex.ru>
2023-05-20 18:43:34 +0400

    make crew page fully responsive

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -7,7 +7,7 @@ export default function PageTitle({ number, title }: PageTitleProps) {
   return (
     <div className='row-start-1 row-end-2 lg:col-start-2 lg:col-end-10 lg:row-start-auto '>
       <h1
-        className='inline-flex md:flex gap-6 px-6
+        className='inline-flex md:flex gap-6
       text-left md:pl-9 lg:pl-0'
       >
         <span className='heading5 font-bold opacity-25'>{number}</span>
