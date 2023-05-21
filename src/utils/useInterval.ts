04a9f505ba33b8a4593c72e8ecaf48e7724ebad5
androidblog <androidblog@yandex.ru>
2023-05-21 19:40:29 +0400

    switch to typescript version of useInterval hook

--- src/utils/useInterval.ts
+++ src/utils/useInterval.ts
@@ -1,29 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 /**
- * Code was taking from the article
- * "Making setInterval Declarative with React Hooks"
- * by Dan Abramov
- * https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+ * Hook was taken from the article
+ * https://usehooks-ts.com/react-hook/use-interval
  * @param callback
  * @param delay
  */
-export function useInterval(callback: () => void, delay: number | null) {
-  const savedCallback = useRef()
 
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback!
+import { useIsomorphicLayoutEffect } from 'usehooks-ts'
+
+export function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback)
+
+  // Remember the latest callback if it changes.
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback
   }, [callback])
 
   // Set up the interval.
   useEffect(() => {
-    function tick() {
-      savedCallback.current()
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return
     }
-    if (delay !== null) {
-      let id = setInterval(tick, delay)
-      return () => clearInterval(id)
-    }
+
+    const id = setInterval(() => savedCallback.current(), delay)
+
+    return () => clearInterval(id)
   }, [delay])
 }
