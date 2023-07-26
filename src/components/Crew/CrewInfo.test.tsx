e68d311c9c71b6d61f77e39e6cad7a4df4340611
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-26 16:08:03 +0400

    upd test

--- src/components/Crew/CrewInfo.test.tsx
+++ src/components/Crew/CrewInfo.test.tsx
@@ -3,7 +3,7 @@ import { CrewInfo } from './CrewInfo'
 import '@testing-library/jest-dom'
 import { Crew } from '@/data/dataTypes'
 
-const crewMember: Crew = {
+const mockCrewMember: Crew = {
   name: 'Crew Member',
   role: 'Crew Role',
   bio: 'Crew Bio',
@@ -15,17 +15,24 @@ const crewMember: Crew = {
 
 describe('CrewInfo', () => {
   it('renders crew role', () => {
-    render(<CrewInfo crewMember={crewMember} />)
+    render(<CrewInfo crewMember={mockCrewMember} />)
     const heading = screen.getByText('Crew Role')
     expect(heading).toBeInTheDocument()
   })
+
   it('renders crew name', () => {
-    render(<CrewInfo crewMember={crewMember} />)
+    render(<CrewInfo crewMember={mockCrewMember} />)
     const heading = screen.getByText('Crew Member')
     expect(heading).toBeInTheDocument()
   })
+  it('renders crew name as h3', () => {
+    render(<CrewInfo crewMember={mockCrewMember} />)
+    const heading = screen.getByRole('heading', { name: 'Crew Member' })
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveClass('heading3')
+  })
   it('renders crew bio', () => {
-    render(<CrewInfo crewMember={crewMember} />)
+    render(<CrewInfo crewMember={mockCrewMember} />)
     const heading = screen.getByText('Crew Bio')
     expect(heading).toBeInTheDocument()
   })
